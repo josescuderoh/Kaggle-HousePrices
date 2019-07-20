0d41fda686b4960f4b92a56bc897e97c641dbe35
josescuderoh <josescuderoh@gmail.com>
2019-07-20 06:58:29 -0400

    Changes before creating pipeline for feature engineering

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import copy
-from sklearn.preprocessing import LabelEncoder
+from scipy.stats import skew
+from sklearn.base import BaseEstimator, TransformerMixin, RegressorMixin, clone
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.pipeline import Pipeline, make_pipeline
 
 #Define plotting style
 plt.style.use('ggplot')
@@ -20,7 +23,7 @@ plt.style.use('ggplot')
 train = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
 
-#Shape of files
+#Shape of dfs
 train.shape
 test.shape
 
@@ -46,6 +49,7 @@ train.drop(train[(train.GrLivArea > 4000)&(train.SalePrice < 300000)].index, inp
 #Merge dataframes
 
 all_data = pd.concat([train,test], sort = False, ignore_index=True)
+all_data.drop(['Id'], axis=1, inplace=True)
 
 null_count = all_data.isnull().sum()
 null_count[(null_count>0)].sort_values(ascending = False)
@@ -84,56 +88,28 @@ clean_data(all_data)
 null_count= all_data.isnull().sum()
 null_count[(null_count>0)].sort_values(ascending = False)
 
+all_data.drop(['SalePrice'], axis=1, inplace = True)  ## Not needed
+
 # =============================================================================
 # Feature engineering 
 # =============================================================================
 
 # Encoding categorical features
+test_df = copy.copy(all_data)
 
-nominal = ["MSSubClass", "MSZoning", "Street", "Alley", "LotShape", "LandContour",
-                "Utilities", "LotConfig", "LandSlope", "Neighborhood", "Condition1",
-                "Condition2", "BldgType", "HouseStyle", "RoofStyle", "RoofMatl",
-                "Exterior1st", "Exterior2nd", "MasVnrType", "Foundation", "BsmtFinType1",
-                "BsmtFinType2", "Heating", "CentralAir", "Electrical",
-                "Functional", "GarageType","GarageFinish", "PavedDrive", "Fence", 
-                "MiscFeature", "SaleType", "SaleCondition"]
+class OrdinalTransformer
 
-test_encoding = copy.copy(all_data)
-
-#Use label encoding for the nominal columns
-
-class MultiColumnLabelEncoder:
-    def __init__(self,columns = None):
-        self.columns = columns # array of column names to encode
-
-    def fit(self,X,y=None):
-        return self # not relevant here
-
-    def transform(self,X):
-        '''
-        Transforms columns of X specified in self.columns using
-        LabelEncoder(). If no columns specified, transforms all
-        columns in X.
-        '''
-        output = X.copy()
-        if self.columns is not None:
-            for col in self.columns:
-                output[col] = LabelEncoder().fit_transform(output[col])
-        else:
-            for colname,col in output.iteritems():
-                output[colname] = LabelEncoder().fit_transform(col)
-        return output
-
-    def fit_transform(self,X,y=None):
-        return self.fit(X,y).transform(X)
-
-
-# Use mapping for ordinal columns
-
-ordinal = ["ExterCond", "ExterQual","BsmtQual", "BsmtCond", "BsmtExposure", 
-           "HeatingQC", "KitchenQual", "FireplaceQu", "GarageQual", "GarageCond", "PoolQC"]
-
+# First map ordinal columns
 def map_ordinal(df):
+    df['LandSlope'] = df['LandSlope'].map({
+            "Gtl": 4,
+            "Mod": 3,
+            "Sev": 2})
+    df['LotShape'] = df['LotShape'].map({
+            "Reg": 4,
+            "IR1": 3,
+            "IR2": 2,
+            "IR3": 1})
     df['ExterCond'] = df['ExterCond'].map({
             "Ex": 5,
             "Gd": 4,
@@ -152,20 +128,36 @@ def map_ordinal(df):
             "TA": 3,
             "Fa": 2,
             "Po": 1,
-            "NA": 0})
+            "None": 0})
     df['BsmtCond'] = df['BsmtCond'].map({
             "Ex": 5,
             "Gd": 4,
             "TA": 3,
             "Fa": 2,
             "Po": 1,
-            "NA": 0})
+            "None": 0})
     df['BsmtExposure'] = df['BsmtExposure'].map({
             "Gd": 4,
             "Av": 3,
             "Mn": 2,
             "No": 1,
-            "NA": 0})
+            "None": 0})
+    df['BsmtFinType1'] = df['BsmtFinType1'].map({
+            "GLQ": 6,
+            "ALQ": 5,
+            "BLQ": 4,
+            "Rec": 3,
+            "LwQ": 2,
+            "Unf": 1,
+            "None": 0})
+    df['BsmtFinType2'] = df['BsmtFinType2'].map({
+            "GLQ": 6,
+            "ALQ": 5,
+            "BLQ": 4,
+            "Rec": 3,
+            "LwQ": 2,
+            "Unf": 1,
+            "None": 0})
     df['HeatingQC'] = df['HeatingQC'].map({
             "Ex": 5,
             "Gd": 4,
@@ -184,60 +176,110 @@ def map_ordinal(df):
             "TA": 3,
             "Fa": 2,
             "Po": 1,
-            "NA": 0})
+            "None": 0})
+    df['GarageFinish'] = df['GarageFinish'].map({
+            "Fin": 3,
+            "RFn": 2,
+            "Unf": 1,
+            "None": 0})
     df['GarageQual'] = df['GarageQual'].map({
             "Ex": 5,
             "Gd": 4,
             "TA": 3,
             "Fa": 2,
             "Po": 1,
-            "NA": 0})
+            "None": 0})
     df['GarageCond'] = df['GarageCond'].map({
             "Ex": 5,
             "Gd": 4,
             "TA": 3,
             "Fa": 2,
             "Po": 1,
-            "NA": 0})
+            "None": 0})
     df['PoolQC'] = df['PoolQC'].map({
             "Ex": 4,
             "Gd": 3,
             "TA": 2,
             "Fa": 1,
-            "NA": 0})
+            "None": 0})
+    df['Fence'] = df['Fence'].map({
+            "GdPrv": 4,
+            "MnPrv": 3,
+            "GdWo": 2,
+            "MnWw": 1,
+            "None": 0})
     
-    print("Encoding terminated")
-    
-    return df
-    
-def encode_categorical(df, nominal):
-    df = MultiColumnLabelEncoder(columns=nominal).fit_transform(df)
-    df = map_ordinal(df)
-    
-    print("Dataframe encoded")
+    print("Mapping terminated")
     return df
 
-all_data = encode_categorical(all_data, nominal)
+##Remember to transform the month
+
+test_df = map_ordinal(test_df)
+
+#Use label encoding for the mapped and non mapped ordinal columns
+    
+#ordinal = ["LotShape", "LandSlope", "OverallQual", "OverallCond", 
+#        "ExterCond", "ExterQual","BsmtQual", "BsmtCond", "BsmtExposure", "BsmtFinType1",
+#        "BsmtFinType2", "HeatingQC", "KitchenQual", "FireplaceQu", "GarageFinish",
+#        "GarageQual", "GarageCond", "PoolQC", "Fence"]
+
+#class MultiColumnLabelEncoder:
+#    def __init__(self,columns = None):
+#        self.columns = columns # array of column names to encode
+#
+#    def fit_transform(self,X):
+#        '''
+#        Transforms columns of X specified in self.columns using
+#        LabelEncoder(). If no columns specified, transforms all
+#        columns in X.
+#        '''
+#        output = X.copy()
+#        if self.columns is not None:
+#            for col in self.columns:
+#                output[col] = LabelEncoder().fit_transform(output[col])
+#        else:
+#            for colname,col in output.iteritems():
+#                output[colname] = LabelEncoder().fit_transform(col)
+#        return output
+#        print("Label encoding terminated")
+
+#test_df = MultiColumnLabelEncoder(columns=ordinal).fit_transform(test_df)
+    
+## Encoding nominal values
+
+## Create class for transforming skewed columns
+class NumericalTransformer(BaseEstimator, TransformerMixin):
+    ##These inherit everything I need to build the class, they have to always be used
+    ##for transformers
+    def __init__(self, skew=0.5):
+        self.skew = skew
+    def fit(self, X, y=None):
+        return self
+    def transform(self, X, y=None):
+        skewness = X.select_dtypes(exclude=['object']).apply(lambda x: skew(x))
+        ## Take note that were we have selected integers as well
+        skewed_idx = skewness[abs(skewness) > self.skew].index  ##Returns the name
+        X[skewed_idx] = np.log1p(X[skewed_idx])
+        return X
+
+# Select nominal
+nominal = ["MSSubClass", "MSZoning", "Street", "Alley", "LandContour",
+                "Utilities", "LotConfig", "Neighborhood", "Condition1",
+                "Condition2", "BldgType", "HouseStyle", "RoofStyle", "RoofMatl",
+                "Exterior1st", "Exterior2nd", "MasVnrType", "Foundation",
+                "Heating", "CentralAir", "Electrical",
+                "Functional", "GarageType","PavedDrive", 
+                "MiscFeature", "SaleType", "SaleCondition"]
+
+
+
+## Transform skewed numeric values
+numerical_transformer = Pipeline(steps=[('transform_numeric', TransformSkewed(skew=1)),
+                                      ('transform_nominal', OneHotEncoder())])
+
+## Should I use get_dummies or OneHotEncoding here???
+## Why not include all trnasformations within the pipline, including the map function
+    
 
 # drop two unwanted columns -->>> WHYYY
 all_data.drop("LotAreaCut",axis=1,inplace=True)
-all_data.drop(['SalePrice'],axis=1,inplace=True)
-
-
-
-# =============================================================================
-# #Exploratory analysis
-# =============================================================================
-
-#Get insights into the y variable
-train.SalePrice.describe()   # Summary
-train.SalePrice.skew()  #->> Prices are highly skewed right
-plt.hist(train.SalePrice, color = "red", bins= 50)
-plt.show()
-
-#Using a logaritmic scale
-np.log(train.SalePrice).skew()  #Skewness
-plt.hist(np.log(train.SalePrice), color = 'red', bins = 50)
-plt.show()    # Seems normally distribuited or symmetric using log
-
-#Find correlation matrix for numeric features
